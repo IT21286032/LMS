e08112ad5dae063393807cb70577cc9613fdc60a
KajeevanJ <it21286032@my.sliit.lk>
2024-08-02 22:00:32 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 5000,
-  mongoURI: process.env.MONGO_URI || 'mongodb+srv://kajee:2001@clustertsms.bloyqbz.mongodb.net/',
-  jwtSecret: process.env.JWT_SECRET || 'mynameisbilla' // Provide a default secret key
+  mongoURI: process.env.MONGO_URI || 'mongodb+srv://kawjevvve:2001vvv@clustertsms.bloyqbz.mongodb.net/',
+  jwtSecret: process.env.JWT_SECRET || 'mynameisbvvwilla' // Provide a default secret key
 };
